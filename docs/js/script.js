883c02129aa95c63554cb9f13120c0f5514d7f8a
Tilak  Sasmal <Tilak@MacBook-Air.local>
2018-07-22 14:03:28 +0530

    added link support

--- docs/js/script.js
+++ docs/js/script.js
@@ -6,14 +6,14 @@ $(document).ready(function(){
   else {
     var toolbarOptions = [
       [{ 'header': 1}, { 'header': 2}],
-      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      ['link'],          // dropdown with defaults from theme
+      //[{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
       ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
       [{ 'align': [] }],
       ['blockquote', 'code-block'],
       [{ 'list': 'ordered'}, { 'list': 'bullet' }],
       [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
       [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
       ['clean']                                         // remove formatting button
     ];
     
